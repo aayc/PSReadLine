aa2a8602ad0890097cc36c9baa4ca196a318df83
Aaron Chan <t-aacha@microsoft.com>
2020-06-10 19:28:48 -0400

    Add explicit access modifiers, change method level types to implicit where possible, regex match Azure commands.

--- PSReadLine/AzSuggestion.cs
+++ PSReadLine/AzSuggestion.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the 2-Clause BSD License.
+
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -6,130 +9,33 @@ using System.Linq;
 using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.PowerShell
 {
     public partial class PSConsoleReadLine
     {
-        const char commandSplitTokens = '|';
-        const string serviceUri = "http://localhost:3000/prediction"; // TODO use real service URI
-        HttpClient client = new HttpClient();
-        List<string> suggestions = new List<string>();
-        List<string> commands = new List<string>();
-        HashSet<string> commandSet = new HashSet<string>();
-        List<Dictionary<string, string>> logs = new List<Dictionary<string, string>>();
-        
+        private const char commandSplitTokens = '|';
+        private const int nHistoryLines = 2;
+        private const string serviceUri = "https://localhost:44386/api/v1/prediction";
+        private Regex azCmdletRegex = new Regex(@"\b\w+-Az\w+\b", RegexOptions.IgnoreCase);
+        private HttpClient client = new HttpClient();
+        private List<string> suggestions = new List<string>();
+        private List<string> commands = new List<string>();
+        private HashSet<string> commandSet = new HashSet<string>();
+        private List<Dictionary<string, string>> logs = new List<Dictionary<string, string>>();
+
         bool waitForPredictions = true;
         bool waitForCommands = true;
 
-        string ProcessHistory()
-        {
-            int nLookBack = 2;
-            List<string> previousLines = new List<string>(nLookBack);
-            for (int i = _history.Count - nLookBack; i < _history.Count; i++)
-            {
-                // If no history available, use start token
-                if (i < 0)
-                {
-                    previousLines.Add("start_of_snippet");
-                    continue;
-                }
-
-                // If history is not related, use start token
-                if (!commandSet.Contains(_history[i].CommandLine.Split(' ')[0]))
-                {
-                    // disregard all previous history and add start token
-                    previousLines = previousLines.Select(_ => "start_of_snippet").ToList();
-                    previousLines.Add("start_of_snippet");
-                    continue;
-                }
-
-                // If history is related, normalize parameters
-                string[] chunks = _history[i].CommandLine.Split(' ');
-                List<List<string>> args = new List<List<string>>();
-                bool isOption = false;
-                for (int j = 1; j < chunks.Length; j++)
-                {
-                    if (chunks[j].StartsWith("-"))
-                    {
-                        isOption = true;
-                        List<string> option = new List<string>();
-                        option.Add(chunks[j]);
-                        args.Add(option);
-                    }
-                    else if (isOption && chunks[j] != "")
-                    {
-                        args.Last().Add("***");
-                        isOption = false;
-                    }
-                }
-                string processedLine = chunks[0] + " " + String.Join(" ", args
-                    .OrderBy(parameter => parameter.First())
-                    .Select(parameter => String.Join(" ", parameter)));
-
-                previousLines.Add(processedLine);
-            }
-            return String.Join("\n", previousLines);
-        }
-
-        string GetTelemetryFilePath()
-        {
-           var telemetryFileName = "suggestion_telemetry.txt";
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                return System.IO.Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                    "Microsoft",
-                    "Windows",
-                    "PowerShell",
-                    "PSReadLine",
-                    telemetryFileName);
-            }
-            else
-            {
-                // PSReadLine can't use Utils.CorePSPlatform (6.0+ only), so do the equivalent:
-                string telemetryPath = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
-
-                if (!String.IsNullOrEmpty(telemetryPath))
-                {
-                    return System.IO.Path.Combine(
-                        telemetryPath,
-                        "powershell",
-                        "PSReadLine",
-                        telemetryFileName);
-                }
-                else
-                {
-                    // Telemetry is data, so it goes into .local/share/powershell folder
-                    var home = Environment.GetEnvironmentVariable("HOME");
-
-                    if (!String.IsNullOrEmpty(home))
-                    {
-                        return System.IO.Path.Combine(
-                            home,
-                            ".local",
-                            "share",
-                            "powershell",
-                            "PSReadLine",
-                            telemetryFileName);
-                    }
-                    else
-                    {
-                        // No HOME, then don't save anything
-                        return "/dev/null";
-                    }
-                }
-            }
-        }
-
-        void RequestPredictions()
+        public void RequestPredictions()
         {
             if (waitForCommands) return;
 
             waitForPredictions = true;
             suggestions.Clear();
-            string historySnippet = ProcessHistory();
-            string requestBody = JsonConvert.SerializeObject(new Dictionary<string, dynamic> {
+            var historySnippet = ProcessHistory();
+            var requestBody = JsonConvert.SerializeObject(new Dictionary<string, dynamic> {
                 { "history", historySnippet },
                 { "clientType", "AzurePowerShell" },
                 { "context", new Dictionary<string, string>{
@@ -141,35 +47,37 @@ namespace Microsoft.PowerShell
             });
             client
                 .PostAsync(serviceUri, new StringContent(requestBody, Encoding.UTF8, "application/json"))
-                .ContinueWith(async (requestTask) => {
-                    string reply = await requestTask.Result.Content.ReadAsStringAsync();
+                .ContinueWith(async (requestTask) =>
+                {
+                    var reply = await requestTask.Result.Content.ReadAsStringAsync();
                     suggestions = JsonConvert.DeserializeObject<List<string>>(reply);
                     waitForPredictions = false;
                 });
         }
 
-        void RequestCommands()
+        public void RequestCommands()
         {
             waitForCommands = true;
             client
                 .GetAsync(serviceUri)
-                .ContinueWith(async (requestTask) => {
-                    string reply = await requestTask.Result.Content.ReadAsStringAsync();
+                .ContinueWith(async (requestTask) =>
+                {
+                    var reply = await requestTask.Result.Content.ReadAsStringAsync();
                     commands = JsonConvert.DeserializeObject<List<string>>(reply);
-                    commandSet = new HashSet<string>(commands);
+                    commandSet = new HashSet<string>(commands.Select(x => x.ToLower()));
                     waitForCommands = false;
                     RequestPredictions();
                 });
         }
 
-        void LogAzSuggestionTelemetry(string submitted)
+        public void LogAzSuggestionTelemetry(string submitted)
         {
             var log = new Dictionary<string, string>();
-            bool usedHistory = false;
-            bool usedSuggestion = false;
+            var usedHistory = false;
+            var usedSuggestion = false;
 
             // Take just the first command
-            submitted = submitted.Trim().Split(' ')[0];
+            submitted = azCmdletRegex.Match(submitted).Value;
             log["submitted"] = submitted;
             log["history"] = ProcessHistory();
             log["suggestions"] = String.Join(Environment.NewLine, suggestions);
@@ -200,7 +108,7 @@ namespace Microsoft.PowerShell
             }
         }
 
-        string GetAzSuggestion(string line)
+        public string GetAzSuggestion(string line)
         {
             var segments = line.Split(commandSplitTokens);
             var lastSegment = segments.Last();
@@ -226,6 +134,95 @@ namespace Microsoft.PowerShell
             else
             {
                 return suggestion;
+            }
+        }
+        private string ProcessHistory()
+        {
+            var previousLines = new List<string>(nHistoryLines);
+            for (int i = _history.Count - nHistoryLines; i < _history.Count; i++)
+            {
+                // If no history available, use start token
+                if (i < 0)
+                {
+                    previousLines.Add("start_of_snippet");
+                    continue;
+                }
+
+                // If history is not related, use start token
+                var azCommand = azCmdletRegex.Match(_history[i].CommandLine);
+                if (!commandSet.Contains(azCommand.Value.ToLower()))
+                {
+                    // disregard all previous history and add start token
+                    previousLines = previousLines.Select(_ => "start_of_snippet").ToList();
+                    previousLines.Add("start_of_snippet");
+                    continue;
+                }
+
+                // If history is related, normalize parameters
+                var line = _history[i].CommandLine.Substring(azCommand.Index);
+                string[] chunks =line.Split(' ');
+                List<List<string>> args = new List<List<string>>();
+                bool isOption = false;
+                for (int j = 1; j < chunks.Length; j++)
+                {
+                    if (chunks[j].StartsWith("-"))
+                    {
+                        isOption = true;
+                        List<string> option = new List<string>();
+                        option.Add(chunks[j]);
+                        args.Add(option);
+                    }
+                    else if (isOption && chunks[j] != "")
+                    {
+                        args.Last().Add("***");
+                        isOption = false;
+                    }
+                }
+                string processedLine = chunks[0] + " " + String.Join(" ", args
+                    .OrderBy(parameter => parameter.First())
+                    .Select(parameter => String.Join(" ", parameter)));
+
+                previousLines.Add(processedLine);
+            }
+            return String.Join("\n", previousLines);
+        }
+
+        private string GetTmpFilePath(string fileName)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return System.IO.Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    "Microsoft",
+                    "Windows",
+                    "PowerShell",
+                    "PSReadLine",
+                    fileName);
+            }
+            else
+            {
+                // PSReadLine can't use Utils.CorePSPlatform (6.0+ only), so do the equivalent:
+                var path = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+
+                if (!String.IsNullOrEmpty(path))
+                {
+                    return System.IO.Path.Combine(path, "powershell", "PSReadLine", fileName);
+                }
+                else
+                {
+                    // Telemetry is data, so it goes into .local/share/powershell folder
+                    var home = Environment.GetEnvironmentVariable("HOME");
+
+                    if (!String.IsNullOrEmpty(home))
+                    {
+                        return System.IO.Path.Combine(home, ".local", "share", "powershell", "PSReadLine", fileName);
+                    }
+                    else
+                    {
+                        // No HOME, then don't save anything
+                        return "/dev/null";
+                    }
+                }
             }
         }
     }
